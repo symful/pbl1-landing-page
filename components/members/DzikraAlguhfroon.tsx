883140bf4a548ab0e57f479689d7da34133405ac
dzikraalghufroon <dzikraalghufroon20@gmail.com>
2026-02-08 23:41:31 +0700

    dzikra profile

--- components/members/DzikraAlguhfroon.tsx
+++ components/members/DzikraAlguhfroon.tsx
@@ -4,15 +4,16 @@ export default function DzikraAlguhfroon() {
   return (
     <MemberCard
       name="Dzikra Alguhfroon"
-      nim="2210004"
-      hobby="DevOps & Security"
-      description="Deep diving into distributed systems and ensuring infrastructure stability and security."
-      role={{ name: "Backend Dev", color: "bg-emerald-600" }}
+      nim="251524041"
+      hobby="belajar"
+      description="Mahasiswa."
+      role={{ name: "Mahasiswa", color: "bg-emerald-600" }}
       cvDetails={[
         "Rust & Go Background",
         "CI/CD Implementation",
         "Cloud Infrastructure",
       ]}
+      profileImg="/images/kemal.jpg"
     />
   );
 }
