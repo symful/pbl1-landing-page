b8f7a749b7aa5f6141a647f8e96e1b8cf894a538
ExuNova <exufrostnova@gmail.com>
2026-02-09 10:45:48 +0700

    Update Data Muhammad Akhtar Khawarizmi
    - Update Data Muhammad Akhtar Khawarizmi

--- components/members/MuhammadAkhtar.tsx
+++ components/members/MuhammadAkhtar.tsx
@@ -4,14 +4,14 @@ export default function MuhammadAkhtar() {
   return (
     <MemberCard
       name="Muhammad Akhtar Khawarizmi"
-      nim="2210002"
-      hobby="UI Research"
+      nim="251524055"
+      hobby="Capture The Flag Enjoyer 🚩"
       description="Bridging the gap between human psychology and digital interfaces with pixel perfection."
-      role={{ name: "UI/UX Designer", color: "bg-rose-600" }}
+      role={{ name: "Technical Writer & Software Tester", color: "bg-rose-600" }}
       cvDetails={[
-        "Figma Specialist",
-        "Visual Interaction Researcher",
-        "Experience Design Architect",
+        "Internship at PT. Bio Farma (Persero) as Back-End Developer",
+        "Software Documentation",
+        "Capture The Flag Enthusiast",
       ]}
     />
   );
