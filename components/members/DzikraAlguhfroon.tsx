b8ce83e45ab3255cf1db998f532a06d84b3906c5
dzikraalghufroon <146427328+jikupon@users.noreply.github.com>
2026-02-09 11:08:35 +0700

    Update DzikraAlguhfroon.tsx

--- components/members/DzikraAlguhfroon.tsx
+++ components/members/DzikraAlguhfroon.tsx
@@ -4,10 +4,10 @@ export default function DzikraAlguhfroon() {
   return (
     <MemberCard
       name="Dzikra Alguhfroon"
-      nim="2210004"
-      hobby="DevOps & Security"
-      description="Deep diving into distributed systems and ensuring infrastructure stability and security."
-      role={{ name: "Backend Dev", color: "bg-emerald-600" }}
+      nim="251524041"
+      hobby="Learn"
+      description="Mahasiswa."
+      role={{ name: "Mahasiswa", color: "bg-emerald-600" }}
       cvDetails={[
         "Rust & Go Background",
         "CI/CD Implementation",
