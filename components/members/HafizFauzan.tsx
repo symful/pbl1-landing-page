13274ec5cd3c8cb4f8a146a89025420a1433ffad
HAFIZ FAUZAN SYAFRUDIN <hafiz.fauzan.tif425@polban.ac.id>
2026-02-09 10:51:01 +0700

    Hafiz Fauzan Syafrudin commit

--- components/members/HafizFauzan.tsx
+++ components/members/HafizFauzan.tsx
@@ -3,15 +3,14 @@ import MemberCard from "../MemberCard.tsx";
 export default function HafizFauzan() {
   return (
     <MemberCard
-      name="Hafiz Fauzan"
-      nim="2210003"
-      hobby="Modern Web Dev"
-      description="Passionate about crafting fluid animations and high-performance frontend applications."
-      role={{ name: "Frontend Dev", color: "bg-blue-600" }}
+      name="Hafiz Fauzan Syafrudin"
+      nim="251524049"
+      hobby="Experiment"
+      description="Saya adalah seorang programmer yang suka bereksperimen dengan teknologi dan hal baru"
+      role={{ name: "IoT Developer", color: "bg-blue-600" }}
       cvDetails={[
-        "React & Fresh Specialist",
-        "Tailwind CSS Expert",
-        "Animation & Motion Design",
+        "Edge Devices Development",
+        "IoT Enthusiast"
       ]}
     />
   );
